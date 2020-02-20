2a5c3fa90061a78c6d4fc4ddc983ea5cc04df2f5
ian <shemian092@gmail.com>
2020-02-20 15:21:12 +0300

    add js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-var livestock =[];
-function animals(a=['cow']){
-    var c=0;
-    for (i=0;i<a.length;i++){
-        if(a[i]===a[c]){
-            console.log(true);
-            livestock.push(a[c]+c);
-            c++
-        }
-        else{
-            console.log(false);
-        }
-    }
-}animals(a=['cow']);
-console.log(livestock);
+var inputs= (foo,for1 ,add24 ,goat99)
+var map= input.map(x=>x +1)
+alert(map)
